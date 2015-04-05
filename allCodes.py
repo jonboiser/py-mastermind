b44bc2d179ba58609ff88c66057c636491d9d9be
Jonathan Boiser <jboiser@gmail.com>
2015-04-04 19:28:36 -0700

    allCodes method for enumerating codes (with optional filter)

---
+++ allCodes.py
@@ -0,0 +1,15 @@
+# When given a set of symbols (usually a range of numbers), and a length,
+# return a list of all possible combinations of those symbols (as tuples).
+# This uses Python's itertools.product method, for convenience, but 
+# coerces the output to a list, rather than keeping it as a generator.
+
+# An optional predicate can be passed into the function to filter it.
+
+# Example:
+# allCodes(range(3), 4) #=> [(0,0,0,0), (0,0,0,1), (0,0,0,2), ..., (2,2,2,2)]
+# allCodes(range(3), 4, lambda x: x[0] == 1) #=> [(1,0,0,0), ..., (1,2,2,2)]
+from itertools import product
+
+def allCodes(symbols, length, pred = lambda x: True):
+	out = filter(pred, product(symbols, repeat = length))
+	return(list(out))
